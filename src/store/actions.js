5086ac2efd16e51b7456b54fe9f351a8fac4677d
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2021-06-11 09:53:42 -0500

    insert name at first line

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,4 @@
+//-------------- react-redux-vs-context\src\store => actions.js --------------
 export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
 export const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODCUT_FROM_CART';
 
